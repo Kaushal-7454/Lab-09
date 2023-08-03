2c9369beb14ebd8c7ca077ec43d5aae3a8acc232
Kaushal Limbachiya <KaushalkumarMaheshkumar.Limbachiya@flemingcollege.ca>
2023-08-03 13:08:18 -0400

    half complted working on it
    this is not final script working on it

--- poke_info_viewer.py
+++ poke_info_viewer.py
@@ -25,13 +25,7 @@ frame_info.grid(row=1, column=0, padx=(20,10), pady=(10,20), sticky=N)
 frame_stats = ttk.LabelFrame(root, text = "stats")
 frame_stats.grid(row=1, column=1, padx=(10,20), pady=(10,10), sticky=N)
 
-label_name = ttk.Entry(root, text = "Pokemon Name")
-label_name.grid(row=0, column=0, padx=(10,5), pady=(10))
-
-enter_name=ttk.Entry(frame_input)
-enter_name.insert(0,'Pikachu')
-enter_name.grid(row=0, column=1)
-
+# TODO: Define button click event handler function
 def handle_button_get_info():
     poke_name = enter_name.get().strip()
     if poke_name == '':
@@ -41,11 +35,36 @@ def handle_button_get_info():
         labe_height_value = [text] = str(poke_info['height']) + 'dm'
         labe_weight_value = [text] = str(poke_info['weight']) + 'hg'
         type_list = [t['Type']['name'].capatalize() for t in poke_info[type]]
-
+        label_types_value.config(text=', '.join(type_list))
 
 # TODO: Populate the user input frame with widgets
 
+label_name = ttk.Entry(root, text = "Pokemon Name")
+label_name.grid(row=0, column=0, padx=(10,5), pady=(10))
 
-# TODO: Define button click event handler function
+enter_name=ttk.Entry(frame_input)
+enter_name.insert(0,'Lucario')
+enter_name.grid(row=0, column=1)
+
+get_info = ttk.Button(frame_input, text="Get Info", command=handle_button_get_info)
+get_info.grid(row=0, column=2, padx=(5, 10))
+
+label_height = ttk.Label(frame_info, text="Height:")
+label_height.grid(row=0, column=0, padx=5, pady=5)
+
+label_height_value = ttk.Label(frame_info, text="", font=("Helvetica", 12, "bold"))
+label_height_value.grid(row=0, column=1, padx=5, pady=5)
+
+label_weight = ttk.Label(frame_info, text="Weight:")
+label_weight.grid(row=1, column=0, padx=5, pady=5)
+
+label_wegt_value = ttk.Label(frame_info, text="", font=("Helvetica", 12, "bold"))
+label_wegt_value.grid(row=1, column=1, padx=5, pady=5)
+
+label_types = ttk.Label(frame_info, text="Types:")
+label_types.grid(row=2, column=0, padx=5, pady=5)
+
+label_types_value = ttk.Label(frame_info, text="", font=("Helvetica", 12, "bold"))
+label_types_value.grid(row=2, column=1, padx=5, pady=5)
 
 root.mainloop()
