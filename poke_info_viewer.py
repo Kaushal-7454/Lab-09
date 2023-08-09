3a0c73c5e8745fd32b0f2b1303d93e7fdd43ed3b
Kaushal Limbachiya <KaushalkumarMaheshkumar.Limbachiya@flemingcollege.ca>
2023-08-09 14:39:41 -0400

    half script still working on it

--- poke_info_viewer.py
+++ poke_info_viewer.py
@@ -15,7 +15,7 @@ from tkinter import messagebox
 root = Tk()
 root.title("Pokemon Information")
 
-# TODO: Create the frames
+# Create the frames
 frame_input = ttk.Frame(root)
 frame_input.grid(row=1, column=0, columnspan=2, pady=(20,10))
 
@@ -25,26 +25,25 @@ frame_info.grid(row=1, column=0, padx=(20,10), pady=(10,20), sticky=N)
 frame_stats = ttk.LabelFrame(root, text = "stats")
 frame_stats.grid(row=1, column=1, padx=(10,20), pady=(10,10), sticky=N)
 
-# TODO: Define button click event handler function
+
+
+# Define button click event handler function
 def handle_button_get_info():
     poke_name = enter_name.get().strip()
     if poke_name == '':
         return
     poke_info = get_pokemon_info(poke_name)
     if poke_info:
-        labe_height_value = [text] = str(poke_info['height']) + 'dm'
-        labe_weight_value = [text] = str(poke_info['weight']) + 'hg'
+        label_height_value = [text] = str(poke_info['height']) + 'dm'
+        label_weight_value = [text] = str(poke_info['weight']) + 'hg'
         type_list = [t['Type']['name'].capatalize() for t in poke_info[type]]
         label_types_value.config(text=', '.join(type_list))
 
-# TODO: Populate the user input frame with widgets
+# Populate the user input frame with widgets
 
 label_name = ttk.Entry(root, text = "Pokemon Name")
 label_name.grid(row=0, column=0, padx=(10,5), pady=(10))
 
-enter_name=ttk.Entry(frame_input)
-enter_name.insert(0,'Lucario')
-enter_name.grid(row=0, column=1)
 
 get_info = ttk.Button(frame_input, text="Get Info", command=handle_button_get_info)
 get_info.grid(row=0, column=2, padx=(5, 10))
